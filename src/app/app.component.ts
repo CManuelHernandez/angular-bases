a81652cbf4133fa1e7d28d9d486a17e4871282a1
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-09-30 16:23:46 +0200

    Contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'Hola Mundo';
-  public counter: number = 0;
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
 }
